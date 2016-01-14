b1e7c1242346a67bd77e5e2ea60c3474657ef111
Rudie Dirkx <github@hotblocks.nl>
2016-01-14 23:35:13 +0100

    autoload try/catch BUG

--- autoload-dev.php
+++ autoload-dev.php
@@ -4,14 +4,13 @@ $autoloader = require __DIR__ . '/autoload-composer.php';
 
 try {
 	$localoadJson = new JsonFile(__DIR__ . '/../composer-locaload.json');
-	try {
-		$locaload = $localoadJson->read();
-		foreach ($locaload as $namespace => $location) {
-			$autoloader->setPsr4($namespace, $location);
-		}
+	$locaload = $localoadJson->read();
+	foreach ($locaload as $namespace => $location) {
+		$autoloader->setPsr4($namespace, $location);
 	}
-	catch (Exception $ex) {}
-
+}
+catch (Exception $ex) {
+	// File doesn't exist, or is unreadable, or has invalid JSON
 }
 
 return $autoloader;
