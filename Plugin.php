2c05fa078c176074504564ff4bad321c79eba144
Rudie Dirkx <github@hotblocks.nl>
2017-02-25 18:56:50 +0100

    #5 +io events to delete localoaded packages

--- Plugin.php
+++ Plugin.php
@@ -31,6 +31,8 @@ class Plugin implements PluginInterface, EventSubscriberInterface {
 	public static function getSubscribedEvents() {
 		return array(
 			ScriptEvents::POST_AUTOLOAD_DUMP => 'onPostAutoloadDump',
+			ScriptEvents::POST_INSTALL_CMD => 'onPostInstall',
+			ScriptEvents::POST_UPDATE_CMD => 'onPostUpdate',
 		);
 	}
 
@@ -49,6 +51,124 @@ class Plugin implements PluginInterface, EventSubscriberInterface {
 				copy($from, $vendorDir . '/autoload.php');
 			}
 		}
+	}
+
+	/**
+	 *
+	 */
+	public function onPostInstall(Event $event) {
+		$this->removeAllReleaseCode($event);
+	}
+
+	/**
+	 *
+	 */
+	public function onPostUpdate(Event $event) {
+		$this->removeAllReleaseCode($event);
+	}
+
+	/**
+	 *
+	 */
+	protected function removeAllReleaseCode($event) {
+		$localoaded = $this->getLocaloadedPackages();
+
+		foreach ($localoaded as $packageNamespace => $dir) {
+			`rm -rf $dir`;
+		}
+
+	}
+
+	/**
+	 *
+	 */
+	protected function getLocaloadedNamespaces() {
+		$meta = array();
+		if (file_exists($file = $this->getRootDir() . '/composer-locaload.json')) {
+			if ($json = @file_get_contents($file)) {
+				$meta = @json_decode($json, true) ?: array();
+			}
+		}
+
+		$namespaces = array_merge(array_keys((array) @$meta['psr-0']), array_keys((array) @$meta['psr-4']));
+		$namespaces = array_map([$this, 'normalizeNamespace'], $namespaces);
+		return $namespaces;
+	}
+
+	/**
+	 *
+	 */
+	protected function getPackageNames() {
+		$locker = $this->composer->getLocker();
+		$lockData = $locker->getLockData();
+
+		$package_names = array_column($lockData['packages'], 'name');
+		return $package_names;
+	}
+
+	/**
+	 *
+	 */
+	protected function normalizeNamespace($namespace) {
+		return str_replace('\\', '/', trim($namespace, '\\/'));
+	}
+
+	/**
+	 *
+	 */
+	protected function getLocaloadedPackages() {
+		$localNamespaces = $this->getLocaloadedNamespaces();
+
+		$localoaded = [];
+		foreach ($this->getPackageNames() as $package_name) {
+			foreach ($this->getPackageAutoload($package_name) as $namespace => $dir) {
+				if (in_array($this->normalizeNamespace($namespace), $localNamespaces)) {
+					$localoaded["$package_name:$namespace"] = $this->getPackageDir($package_name) . '/'. trim($dir, '\\/');
+				}
+			}
+		}
+
+		return $localoaded;
+	}
+
+	/**
+	 *
+	 */
+	protected function getPackageAutoload($package_name) {
+		$file = $this->getPackageDir($package_name) . '/composer.json';
+		$data = json_decode(file_get_contents($file), true);
+
+		$namespaces = [];
+		foreach ((array) @$data['autoload']['psr-0'] as $namespace => $dir) {
+			$namespaces[$namespace] = $dir . '/' . str_replace('\\', '/', $namespace);
+		}
+		foreach ((array) @$data['autoload']['psr-4'] as $namespace => $dir) {
+			$namespaces[$namespace] = $dir;
+		}
+
+		return $namespaces;
+	}
+
+	/**
+	 *
+	 */
+	protected function getPackageDir($package_name) {
+		return $this->getVendorDir() . '/' . $package_name;
+	}
+
+	/**
+	 *
+	 */
+	protected function getVendorDir() {
+		$config = $this->composer->getConfig();
+		return $config->get('vendor-dir');
+	}
+
+	/**
+	 *
+	 */
+	protected function getRootDir() {
+		return dirname($this->getVendorDir());
 	}
 
 }
